4db957ac08f3a017f38ae9fd7b8d22aa8010a95f
astutz96 <astutz96.development@protonmail.com>
2022-10-12 22:01:52 -0400

    Implementing first iteration of recyclerview with hardcoded data set and simple List_item layout.

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.example.affirmations
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.adapter.ItemAdapter
 import com.example.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //init data set
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerview = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerview.adapter = ItemAdapter(this, myDataset)
+
+        /**
+         * Use this setting to improve performance if you know that changes in the content
+         * do not change the layout size of the RecyclerView
+        */
+        recyclerview.setHasFixedSize(true)
 
 //        val textView: TextView = findViewById(R.id.textview)
 //        textView.text = Datasource().loadAffirmations().size.toString()
